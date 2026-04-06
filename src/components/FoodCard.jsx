76d94256bceb33c18bd944938d43519ca0b6773b
Shaadma salam <nefisashaadmasalam@gmail.com>
2026-04-06 22:19:51 +0530

    styled food card

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,12 +1,28 @@
 function FoodCard({ product }) {
   return (
-    <div>
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "15px",
+        margin: "15px",
+        borderRadius: "12px",
+        backgroundColor: "#1e1e1e",
+        color: "white",
+        width: "250px",
+        textAlign: "center"
+      }}
+    >
       <h3>{product.product_name || "No Name"}</h3>
       <p>{product.brands || "Unknown"}</p>
 
       <img
-        src={product.image_url || "https://via.placeholder.com/100"}
-        width="100"
+        src={product.image_url || "https://via.placeholder.com/150"}
+        alt="food"
+        style={{
+          width: "100%",
+          borderRadius: "10px",
+          marginBottom: "10px"
+        }}
       />
 
       <p>Energy: {product.nutriments?.energy || "N/A"}</p>
